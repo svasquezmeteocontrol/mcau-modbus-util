e273cd4ed8efd21ab7773e231c97c963638a55a9
Sebastian <sebastianvg@gmail.com>
2020-04-27 11:31:44 +1000

    Adjustment to configure the modbus clients

--- src/main/java/com/meteocontrol/modbus/configuration/ModbusTcpMasterConfigUtil.java
+++ src/main/java/com/meteocontrol/modbus/configuration/ModbusTcpMasterConfigUtil.java
@@ -1,21 +1,34 @@
 package com.meteocontrol.modbus.configuration;
 
+import java.util.HashMap;
+
 import com.digitalpetri.modbus.master.ModbusTcpMaster;
 import com.digitalpetri.modbus.master.ModbusTcpMasterConfig;
 
 public class ModbusTcpMasterConfigUtil {
 
-	public static ModbusTcpMaster configModbusTcpMasterConnected(String ipAddress) {
-		ModbusTcpMasterConfig modbusConfig = new ModbusTcpMasterConfig.Builder(ipAddress).build();
-		ModbusTcpMaster modbusTcpMaster = new ModbusTcpMaster(modbusConfig);
-		
-		
+	private static HashMap<String, ModbusTcpMaster> modbusTcpMasterHM = new HashMap<String, ModbusTcpMaster>();;
+
+	public static ModbusTcpMaster getModbusTcpMaster(String ipAddress) {
+
+		ModbusTcpMaster modbusTcpMaster = modbusTcpMasterHM.get(ipAddress);
+
+		if (modbusTcpMaster == null) {
+			ModbusTcpMasterConfig modbusConfig = new ModbusTcpMasterConfig.Builder(ipAddress).build();
+			modbusTcpMaster = new ModbusTcpMaster(modbusConfig);
+			modbusTcpMasterHM.put(ipAddress, modbusTcpMaster);
+		}
+
 		return modbusTcpMaster;
 	}
-	
-	public static void connect(ModbusTcpMaster modbusTcpMaster) {
-//		CompletableFuture<ModbusTcpMaster> futureConnection = modbusTcpMaster.connect();
-		modbusTcpMaster.connect();
+
+	public static void clearModbusTcpMasterHM() {
+		modbusTcpMasterHM.values().forEach(master -> {
+			master.disconnect();
+		});
+		modbusTcpMasterHM.clear();
+		modbusTcpMasterHM = null;
+		modbusTcpMasterHM = new HashMap<String, ModbusTcpMaster>();
 	}
 
 }
